577e6640cee5dbc2dcf596c9188bde13402b884c
Kingdaro <kingdaro@gmail.com>
2016-04-16 12:58:53 -0400

    and back to the beginning!

--- luna/tokenize.lua
+++ luna/tokenize.lua
@@ -6,53 +6,42 @@ return function(content)
   local tokens = {}
   local current = 1
 
+  local function pass(pattern)
+    local location, value = content:match('()(' .. pattern .. ')', current)
+    if location == current then
+      current = current + #value
+      return value
+    end
+  end
+
+  local function match(pattern, tokentype)
+    local value = pass(pattern)
+    if value then
+      insert(tokens, { type = tokentype, value = value })
+      return true
+    end
+  end
+
   while current < #content do
     repeat
       -- white space
-      local location, space = content:match('()(%s+)', current)
-      if location == current then
-        current = current + #space
-        break
-      end
+      if pass('%s+') then break end
 
       -- comments
-      local location, comment = content:match('()(%-%-[^\r\n]*)', current)
-      if location == current then
-        current = current + #comment
-        break
-      end
+      if pass('%-%-%[%[.-%]%]') then break end
+      if pass('%-%-[^\r\n]*') then break end
 
       -- hex numbers
-      local location, hex = content:match('()(0x[%dA-Fa-f]+)', current)
-      if location == current then
-        current = current + #hex
-        insert(tokens, { type = 'literal-number', value = hex })
-        break
-      end
+      if match('0x[%dA-Fa-f]+', 'literal-number') then break end
 
       -- numbers
-      local location, number = content:match('()(%d*%.?%d+)', current)
-      if location == current then
-        current = current + #number
-        insert(tokens, { type = 'literal-number', value = number })
-        break
-      end
+      if match('%d*%.?%d+', 'literal-number') then break end
 
       -- names
-      local location, name = content:match('()([%a_][%w_]*)', current)
-      if location == current then
-        current = current + #name
-        insert(tokens, { type = 'literal-name', value = name })
-        break
-      end
+      if match('[%a_][%w_]*', 'literal-name') then break end
 
       -- strings
-      local location, str = content:match('()(%b"")')
-      if location == current then
-        current = current + #str
-        insert(tokens, { type = 'literal-string', value = str })
-        break
-      end
+      if match('%b""', 'literal-string') then break end
 
       error(format('unexpected character %q at %d', content:sub(current, current), current), 0)
     until true
