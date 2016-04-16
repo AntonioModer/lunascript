9de8106fee6729420fa78ee9cd73ce965bee812b
Kingdaro <kingdaro@gmail.com>
2016-04-16 16:44:59 -0400

    somewhat better proper parsing somehow maybe?

--- luna/parse.lua
+++ luna/parse.lua
@@ -2,39 +2,52 @@ local insert = table.insert
 local concat = table.concat
 local format = string.format
 
-local function parseLiteral(tokentype, token)
-  if token.type == tokentype then
-    return { type = tokentype, token.value }
-  end
-end
 
-local function parseExpression(...)
-  return parseLiteral('literal-number', ...)
-  or parseLiteral('literal-string', ...)
-  or parseLiteral('literal-vararg', ...)
-  or parseLiteral('literal-constant', ...)
-end
+return function(tokens)
+  local parseExpression
+  local parseLiteral
+  local parseInfixedExpression
+  local parseBlock
 
-local function parseBlock(current, ...)
-  local tokens = {...}
-  local result = { type = 'block' }
-  local node
-
-  while current <= #tokens do
-    node = parseExpression(unpack(tokens, current))
-    if node then
-      table.insert(result, node)
-      current = current + #node
-    else
-      error(format('unexpected token %q at %d', tokens[current].value, current))
+  function parseLiteral(tokentype, current, token)
+    if token.type == tokentype then
+      return current + 1, { type = tokentype, token.value }
     end
   end
 
-  return current, result
-end
+  function parseInfixedExpression(open, ...)
+    -- if open.type == 'infix-open' or open.type == 'infix-close' then
+    --   return { type = 'infix', open.value }
+    -- end
+  end
 
+  function parseExpression(...)
+    local pos, node = parseLiteral('literal-number', ...)
+    if not node then pos, node = parseLiteral('literal-string', ...) end
+    if not node then pos, node = parseLiteral('literal-vararg', ...) end
+    if not node then pos, node = parseLiteral('literal-constant', ...) end
+    if not node then pos, node = parseLiteral('literal-name', ...) end
+    return pos, node
+  end
 
-return function(tokens)
-  local _, tree = parseBlock(1, unpack(tokens))
-  return tree
+  function parseBlock(current, ...)
+    local tokens = {...}
+    local result = { type = 'block' }
+    local pos, node
+
+    while current <= #tokens do
+      pos, node = parseExpression(current, unpack(tokens, current))
+      if node then
+        table.insert(result, node)
+        current = pos
+      else
+        error(format('unexpected token %q at %d', tokens[current].value, current))
+      end
+    end
+
+    return current, result
+  end
+
+  local _, ast = parseBlock(1, unpack(tokens))
+  return ast
 end
