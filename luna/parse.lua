efe3ad9021a88d7a8158bcb1f70ac5e9c897e82b
Kingdaro <kingdaro@gmail.com>
2016-05-03 21:05:40 -0400

    parse lines individually

--- luna/parse.lua
+++ luna/parse.lua
@@ -1,12 +1,41 @@
 local parse = {}
 
-function parse.lines(source)
+function parse.lex(source)
   local lines = {}
+  local linenum = 1
+
   for line in source:gmatch('[^\r\n]+') do
     if line:match('%s+') ~= line then
-      table.insert(lines, line)
+      local indentation = #line:match('^%s+')
+      local tokens = {}
+      local current = level
+
+      -- local function match(pattern, tokentype)
+      --   local position, value, capture = line:match('()(' .. pattern .. ')', current)
+      --   if position == current then
+      --     value = capture or value
+      --     local token = { type = tokentype, value = value }
+      --     table.insert(tokens, token)
+      --     current = current + #value
+      --     return token
+      --   end
+      -- end
+
+      -- while current <= #line do
+      --   if true then
+      --   else
+      --     local errformat = "[%s] Syntax error: unknown character %q (line %d col %d)"
+      --     local errmsg = errformat:format(identity, line:sub(current, current), linenum, current)
+      --     error(errmsg, 0)
+      --   end
+      -- end
+
+      local lineinfo = { tokens = tokens, indentation = indentation, num = linenum }
+      table.insert(lines, lineinfo)
+      linenum = linenum + 1
     end
   end
+
   return lines
 end
 
--- tests/parse_spec.lua
+++ tests/parse_spec.lua
@@ -1,7 +1,11 @@
 local parse = require 'luna.parse'
 
 describe('parser', function()
-  it('parses lines, ignoring unnecessary whitespace', function()
-    assert.are.same(parse.lines 'hello\nworld\n  \n\ntest\n', {'hello','world','test'})
+  it('ignores empty source', function()
+    assert.are.same(parse.lex(''), {})
+  end)
+
+  it('ignores whitespace-only lines', function()
+    assert.are.same(parse.lex('  \n\n  \n'), {})
   end)
 end)
