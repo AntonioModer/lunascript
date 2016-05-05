13e9b8110c75757e2484bbddf44fd4eef2e3ae02
Kingdaro <kingdaro@gmail.com>
2016-05-05 19:39:36 -0400

    added spec for transform

--- tests/transform_spec.lua
+++ tests/transform_spec.lua
@@ -1,3 +1,30 @@
 local lex = require 'luna.lex'
 local parse = require 'luna.parse'
 local transform = require 'luna.transform'
+
+local source = [[
+let hello = 'world'
+let a, b, c = 1, 2, 3
+]]
+
+local ast = transform(parse(lex(source)))
+
+describe('luna.transform', function()
+  it('transforms luna into a lua AST', function()
+    assert.are.same({
+      body = { {
+          namelist = { "a", "hello", "c", "b" },
+          type = "local"
+        }, {
+          explist = { "'world'" },
+          namelist = { "hello" },
+          type = "assign"
+        }, {
+          explist = { "1", "2", "3" },
+          namelist = { "a", "b", "c" },
+          type = "assign"
+        } },
+      type = "block"
+    }, ast)
+  end)
+end)
