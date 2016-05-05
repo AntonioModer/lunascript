47094780f2b92f5d80df1ef92333baac242ca419
Kingdaro <kingdaro@gmail.com>
2016-05-05 03:53:24 -0400

    created lexer, added spec

--- luna/tokens.lua
+++ luna/tokens.lua
@@ -1,79 +1,79 @@
 return {
   -- comments
-  { type = 'comment',    match = '%-%-[^\r\n]+', ignore = true },
+  { type = 'comment', pattern = '%s*%-%-%-.-%-%-%-%s*', ignore = true }, -- multiline
+  { type = 'comment', pattern = '%s*%-%-[^\r\n]*',      ignore = true }, -- single line
 
   -- whitespace
-  { type = 'space',             match = '[ \t]+'  }, -- space character
-  { type = 'line-break',        match = '[\r\n]+' }, -- line break character - treat multiple as a single break
-  { type = 'line-continuation', match = '\\%s+'   }, -- line continuation: continue current line to next, ignores indentation
+  { type = 'space',             pattern = '[ \t]+'  }, -- space character
+  { type = 'line-break',        pattern = '[\r\n]+' }, -- line break character - treat multiple as a single break
+  { type = 'line-continuation', pattern = '\\%s+'   }, -- line continuation: continue current line to next, ignores indentation
 
-  { type = 'number', match = '0x%x+' },               -- hex number
-  { type = 'number', match = '%d*%.?%d+e%d+' },       -- sci notation (short form)
-  { type = 'number', match = '%d*%.?%d+E[%+%-]%d+' }, -- sci notation (long form)
-  { type = 'number', match = '%d*%.?%d+' },           -- decimal number
+  { type = 'number', pattern = '0x%x+' },               -- hex number
+  { type = 'number', pattern = '%d*%.?%d+e%d+' },       -- sci notation (short form)
+  { type = 'number', pattern = '%d*%.?%d+E[%+%-]%d+' }, -- sci notation (long form)
+  { type = 'number', pattern = '%d*%.?%d+' },           -- decimal number
 
-  { type = 'string', head = '"',   body = {'\\.', '.'}, tail = '"'   }, -- double quoted string
-  { type = 'string', head = "'",   body = {'\\.', '.'}, tail = "'"   }, -- single quoted string
-  { type = 'string', head = '[[',  body = {'\\.', '.'}, tail = ']]'  }, -- multiline string
-  { type = 'string', head = '"""', body = {'\\.', '.'}, tail = '"""' }, -- doc string
+  { type = 'string', head = '"""',   body = {'\\.', '.'}, tail = '"""'  }, -- multiline string
+  { type = 'string', head = '"',     body = {'\\.', '.'}, tail = '"'    }, -- double quoted string
+  { type = 'string', head = "'",     body = {'\\.', '.'}, tail = "'"    }, -- single quoted string
 
   -- symbols, grouped by char length
-  { type = 'vararg', match = '%.%.%.' },
+  { type = 'vararg', pattern = '%.%.%.' },
 
-  { type = 'floor-divide',  match = '//'   },
-  { type = 'concat',        match = '%.%.' },
-  { type = 'equality',      match = '==' },
-  { type = 'greater-equal', match = '>=' },
-  { type = 'less-equal',    match = '<=' },
+  { type = 'floor-divide',  pattern = '//'   },
+  { type = 'concat',        pattern = '%.%.' },
+  { type = 'equality',      pattern = '==' },
+  { type = 'greater-equal', pattern = '>=' },
+  { type = 'less-equal',    pattern = '<=' },
 
-  { type = 'plus',           match = '%+' },
-  { type = 'minus',          match = '%-' },
-  { type = 'multiply',       match = '%*' },
-  { type = 'divide',         match = '/'  },
-  { type = 'modulo',         match = '%%' },
-  { type = 'len',            match = '#'  },
-  { type = 'power',          match = '^'  },
-  { type = 'greater',        match = '>'  },
-  { type = 'less',           match = '<'  },
-  { type = 'dot',            match = '%.' },
-  { type = 'colon',          match = ':'  },
-  { type = 'assign',         match = '='  },
-  { type = 'comma',          match = ','  },
-  { type = 'open-bracket',   match = '%[' },
-  { type = 'closed-bracket', match = '%]' },
-  { type = 'open-paren',     match = '%(' },
-  { type = 'closed-paren',   match = '%)' },
-  { type = 'open-brace',     match = '{'  },
-  { type = 'closed-brace',   match = '}'  },
+  { type = 'plus',           pattern = '%+' },
+  { type = 'minus',          pattern = '%-' },
+  { type = 'multiply',       pattern = '%*' },
+  { type = 'divide',         pattern = '/'  },
+  { type = 'modulo',         pattern = '%%' },
+  { type = 'len',            pattern = '#'  },
+  { type = 'power',          pattern = '^'  },
+  { type = 'greater',        pattern = '>'  },
+  { type = 'less',           pattern = '<'  },
+  { type = 'dot',            pattern = '%.' },
+  { type = 'colon',          pattern = ':'  },
+  { type = 'assign',         pattern = '='  },
+  { type = 'comma',          pattern = ','  },
+  { type = 'open-bracket',   pattern = '%[' },
+  { type = 'closed-bracket', pattern = '%]' },
+  { type = 'open-paren',     pattern = '%(' },
+  { type = 'closed-paren',   pattern = '%)' },
+  { type = 'open-brace',     pattern = '{'  },
+  { type = 'closed-brace',   pattern = '}'  },
 
   -- keywords
-  { type = 'if',       find = 'if',       match = '%l+' },
-  { type = 'elseif',   find = 'elseif',   match = '%l+' },
-  { type = 'else',     find = 'else',     match = '%l+' },
-  { type = 'then',     find = 'then',     match = '%l+' },
-  { type = 'while',    find = 'while',    match = '%l+' },
-  { type = 'do',       find = 'do',       match = '%l+' },
-  { type = 'local',    find = 'local',    match = '%l+' },
-  { type = 'global',   find = 'global',   match = '%l+' },
-  { type = 'switch',   find = 'switch',   match = '%l+' },
-  { type = 'when',     find = 'when',     match = '%l+' },
-  { type = 'for',      find = 'for',      match = '%l+' },
-  { type = 'in',       find = 'in',       match = '%l+' },
-  { type = 'of',       find = 'of',       match = '%l+' },
-  { type = 'to',       find = 'to',       match = '%l+' },
-  { type = 'by',       find = 'by',       match = '%l+' },
-  { type = 'every',    find = 'every',    match = '%l+' },
-  { type = 'import',   find = 'import',   match = '%l+' },
-  { type = 'continue', find = 'continue', match = '%l+' },
-  { type = 'break',    find = 'break',    match = '%l+' },
-  { type = 'and',      find = 'and',      match = '%l+' },
-  { type = 'or',       find = 'or',       match = '%l+' },
-  { type = 'not',      find = 'not',      match = '%l+' },
-  { type = 'true',     find = 'true',     match = '%l+' },
-  { type = 'false',    find = 'false',    match = '%l+' },
-  { type = 'is',       find = 'is',       match = '%l+' },
-  { type = 'isnt',     find = 'isnt',     match = '%l+' },
+  { type = 'if',       find = 'if',       pattern = '%l+' },
+  { type = 'elseif',   find = 'elseif',   pattern = '%l+' },
+  { type = 'else',     find = 'else',     pattern = '%l+' },
+  { type = 'then',     find = 'then',     pattern = '%l+' },
+  { type = 'while',    find = 'while',    pattern = '%l+' },
+  { type = 'do',       find = 'do',       pattern = '%l+' },
+  { type = 'local',    find = 'local',    pattern = '%l+' },
+  { type = 'global',   find = 'global',   pattern = '%l+' },
+  { type = 'switch',   find = 'switch',   pattern = '%l+' },
+  { type = 'when',     find = 'when',     pattern = '%l+' },
+  { type = 'for',      find = 'for',      pattern = '%l+' },
+  { type = 'in',       find = 'in',       pattern = '%l+' },
+  { type = 'of',       find = 'of',       pattern = '%l+' },
+  { type = 'to',       find = 'to',       pattern = '%l+' },
+  { type = 'by',       find = 'by',       pattern = '%l+' },
+  { type = 'every',    find = 'every',    pattern = '%l+' },
+  { type = 'import',   find = 'import',   pattern = '%l+' },
+  { type = 'continue', find = 'continue', pattern = '%l+' },
+  { type = 'break',    find = 'break',    pattern = '%l+' },
+  { type = 'and',      find = 'and',      pattern = '%l+' },
+  { type = 'or',       find = 'or',       pattern = '%l+' },
+  { type = 'not',      find = 'not',      pattern = '%l+' },
+  { type = 'true',     find = 'true',     pattern = '%l+' },
+  { type = 'false',    find = 'false',    pattern = '%l+' },
+  { type = 'is',       find = 'is',       pattern = '%l+' },
+  { type = 'isnt',     find = 'isnt',     pattern = '%l+' },
 
   -- lua names
-  { type = 'name', match = '[%a_][%w_]*' },
+  { type = 'name', pattern = '[%a_][%w_]*' },
 }
